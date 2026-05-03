cce3b27d4b4213f5cd7d80be9bfdde21b55b491a
brabravol <bra.bravol@duocuc.cl>
2026-05-02 20:56:17 -0400

    configuracion inicial

---
+++ src/test/java/com/smarth/solutions/gateway/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarth.solutions.gateway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
