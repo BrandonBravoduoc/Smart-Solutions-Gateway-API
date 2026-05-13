a296c4b6626d826159ddc98058bbc2d2e75a39df
brabravol <bra.bravol@duocuc.cl>
2026-05-12 20:53:21 -0400

    configuracion_gateway_valida_token_y_permite_trafico

---
+++ src/main/java/com/smarth/solutions/gateway/api/config/JwtGatewayFilter.java
@@ -0,0 +1,73 @@
+package com.smarth.solutions.gateway.api.config;
+
+import com.smarth.solutions.gateway.api.service.JwtService;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class JwtGatewayFilter implements GlobalFilter, Ordered {
+
+    private final JwtService jwtService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+        
+        if (request.getMethod().name().equals("OPTIONS")) {
+        return chain.filter(exchange);
+        }
+
+
+        if (path.contains("/api/v1/auth") || 
+            path.contains("/api/v1/regions") || 
+            path.contains("/api/v1/communes") || 
+            path.contains("/api/v1/addresses") || 
+            path.contains("/swagger-ui") || 
+            path.contains("/v3/api-docs")){
+            return chain.filter(exchange);
+        }
+
+        HttpCookie cookie = request.getCookies().getFirst("accessToken");
+        
+        if (cookie == null || cookie.getValue().isEmpty()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete(); 
+        }
+
+        String token = cookie.getValue();
+
+        if (!jwtService.isTokenValid(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String email = jwtService.extractClaim(token, Claims::getSubject);
+        Long id = jwtService.extractClaim(token, claims -> claims.get("userId", Long.class));
+        String role = jwtService.extractClaim(token, claims -> claims.get("role", String.class));
+
+        ServerHttpRequest mutatedRequest = request.mutate()
+                .header("X-User-Id", String.valueOf(id))
+                .header("X-User-Email", email)
+                .header("X-User-Role", role)
+                .build();
+
+        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+    }
+
+    @Override
+    public int getOrder() {
+        return 1; 
+    }
+}
---
+++ src/main/java/com/smarth/solutions/gateway/api/service/JwtService.java
@@ -0,0 +1,48 @@
+package com.smarth.solutions.gateway.api.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKeyString;
+
+        
+    
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = this.secretKeyString.getBytes(StandardCharsets.UTF_8);
+                return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !extractClaim(token, Claims::getExpiration).before(new Date());
+        } catch (Exception e) {
+            return false; 
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
