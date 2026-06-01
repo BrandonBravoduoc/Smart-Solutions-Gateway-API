e6f779b2ae40b8ded4a84bf6b04182ca172c6acc
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-05-31 20:44:53 -0400

    fix: corrección de endpoint

--- src/main/java/com/smarth/solutions/gateway/api/controller/HealthController.java
+++ src/main/java/com/smarth/solutions/gateway/api/controller/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/healths")
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping
     public String health() {
         return "OK";
     }
