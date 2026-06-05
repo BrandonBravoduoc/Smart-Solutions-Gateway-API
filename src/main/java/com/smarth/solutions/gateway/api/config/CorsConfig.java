d03a8d9487784d19ef49191d2a3806604c913d98
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-05 12:24:50 -0400

    Add CloudFront URL to allowed CORS origins

--- src/main/java/com/smarth/solutions/gateway/api/config/CorsConfig.java
+++ src/main/java/com/smarth/solutions/gateway/api/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        corsConfig.setAllowedOrigins(Arrays.asList("https://d1cvogky5serqv.cloudfront.net"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
