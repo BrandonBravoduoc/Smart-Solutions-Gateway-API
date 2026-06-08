5c5fbc1b7f66096061087a82a1cc932aba731684
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-08 19:27:10 -0400

    Fix gateway CORS configuration to use allowedOriginPatterns with credentials

--- src/main/java/com/smarth/solutions/gateway/api/config/CorsConfig.java
+++ src/main/java/com/smarth/solutions/gateway/api/config/CorsConfig.java
@@ -14,7 +14,6 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("https://d1cvogky5serqv.cloudfront.net"));
         corsConfig.setAllowedOriginPatterns(Arrays.asList("https://*.cloudfront.net"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
