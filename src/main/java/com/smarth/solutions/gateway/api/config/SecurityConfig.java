64ce6b8f6e1804ed691d34457a25e0f859d2c6e9
BenjaminMarin4444 <bra.bravol@duocuc.cl>
2026-06-11 21:48:51 -0400

    fix cors

--- src/main/java/com/smarth/solutions/gateway/api/config/SecurityConfig.java
+++ src/main/java/com/smarth/solutions/gateway/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.smarth.solutions.gateway.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,7 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-            .cors(cors -> cors.disable())  
+            .cors(Customizer.withDefaults())  
             .csrf(csrf -> csrf.disable())
             .authorizeExchange(exchanges -> exchanges
                 .anyExchange().permitAll()
