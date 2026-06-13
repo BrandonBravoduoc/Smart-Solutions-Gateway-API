2e3cecd356052890513a19ced2cc32cd13450b31
Nombre Apellido <be.marinv@duocuc.cl>
2026-06-13 05:52:13 -0400

    feat: implementacion payment api

--- src/main/java/com/smarth/solutions/gateway/api/config/RouteConfig.java
+++ src/main/java/com/smarth/solutions/gateway/api/config/RouteConfig.java
@@ -12,12 +12,18 @@ public class RouteConfig {
     @Value("${auth.service.uri:http://smartsolutions-auth-alb-1154380074.sa-east-1.elb.amazonaws.com}")
     private String authServiceUri;
 
+    @Value("${payment.service.uri:http://localhost:3000}")
+    private String paymentServiceUri;
+
     @Bean
     public RouteLocator additionalRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-admin", r -> r
                         .path("/api/v1/admin/**", "/api/v1/roles/**", "/api/v1/healths/**")
                         .uri(authServiceUri))
+                .route("payment-service", r -> r
+                        .path("/api/payments/**")
+                        .uri(paymentServiceUri))
                 .build();
     }
 }
